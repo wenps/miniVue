0826fd3e3e4b0afe336da6517c91a8e4eccc4bc6
xiaoshangit <2534491497@qq.com>
2023-06-21 16:19:18 +0800

    feat:插值类型，text类型，元素类型parse整合

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -9,7 +9,7 @@ export function baseParse(content:string) {
     // 创建一个全局上下文对象，包裹传进来的 content
     const context = createParserContext(content)
 
-    return createRoot(parseChildren(context))
+    return createRoot(parseChildren(context, []))
 }
 
 // 封装content逻辑
@@ -25,28 +25,34 @@ function createRoot(children) {
         children
     }
 }
-
+// elementTagStack 元素标签栈，用来存储元素标签，判断标签是否闭合等关系
 // 抽离children节点逻辑
-function parseChildren(context) {
+function parseChildren(context, elementTagStack ) {
     const nodes:any = [] // 创建节点列表
-    let node;
-    const s = context.source
-    // 判断是否插值类型节点
-    if(s.startsWith('{{')) {
-        node = parseInterpolation(context) // 解析插值类型
-    } 
-    // 判断是否元素类型节点，是否以 < 开头，第二个字段是否a-z中的字符
-    else if (s[0] === '<') {
-        if(/[a-z]/i.test(s[1])) {
-            node = parseElement(context) // 解析元素类型
+
+    // 循环解析节点内容，当context推进完之后结束 或者 如果是解析元素标签时，遇到闭合标签也要结束
+    while (!isEnd(context, elementTagStack )) {
+
+        let node;
+        const s = context.source
+        // 判断是否插值类型节点
+        if(s.startsWith('{{')) {
+            node = parseInterpolation(context) // 解析插值类型
+        } 
+        // 判断是否元素类型节点，是否以 < 开头，第二个字段是否a-z中的字符
+        else if (s[0] === '<') {
+            if(/[a-z]/i.test(s[1])) {
+                node = parseElement(context, elementTagStack) // 解析元素类型
+            }
         }
-    }
-    // 处理text类型节点，默认情况下如果没有命中元素也没有命中插值即取不到node，默认为text
-    if(!node) { 
-        node = parseText(context)
+        // 处理text类型节点，默认情况下如果没有命中元素也没有命中插值即取不到node，默认为text
+        if(!node) { 
+            node = parseText(context)
+        }
+
+        nodes.push(node) // 设置节点列表
     }
 
-    nodes.push(node) // 设置节点列表
     return nodes
 }
 
@@ -73,9 +79,24 @@ function parseInterpolation(context) {
 }
 
 // 解析元素类型
-function parseElement(context) {
-    const element = parseTag(context, TagType.start) // 处理开头标签
-    parseTag(context, TagType.end) // 处理结束标签
+function parseElement(context, elementTagStack ) {
+    const element:any = parseTag(context, TagType.start) // 处理开头标签
+
+    // 标签入栈
+    elementTagStack.push(element)
+
+    element.children = parseChildren(context, elementTagStack) // 读取元素类型标签内容，递归解析， 如： <div>xx, {{a}}</div> 上一步推进一个标签 得到 xx, {{a}}</div>，递归解析这个字符串
+    // 同时要传入当前元素的标签，用于处理children递归的结束条件
+
+    // 递归解析完，标签出栈
+    elementTagStack.pop()
+
+    // 判断结束标签和当前标签是否相同，如果相同则，才能处理这个结束标签做推进
+    if (context.source.slice(2, 2 + element.tag.length) == element.tag) {
+      parseTag(context, TagType.end) // 处理结束标签
+    } else {
+        throw new Error("标签不闭合");   
+    }
     return element
     
 }
@@ -98,8 +119,19 @@ function parseTag(context: any, type: TagType) {
 
 // 解析text类型
 function parseText(context: any): any {
+
+    let endIndex = context.source.length // 默认整个字符串都是text类型
+    let endTokens = ["<", "{{"]
+
+    for (let i = 0; i < endTokens.length; i++) {
+        let index = context.source.indexOf(endTokens[i]) // 判断当前是否出现了非text类型的标识，如果出现则0到当前下标则为text类型节点内容
+        if(index != -1 && endIndex > index) { // 取最左的下标
+            endIndex = index
+        }
+    }
+
     // 1.获取text内容
-    const content = context.source.slice(0, context.source.length)
+    const content = context.source.slice(0, endIndex)
     // 2.推进
     advanceBy(context, content.length)
 
@@ -114,3 +146,24 @@ function advanceBy(context:any, length:number) {
     context.source = context.source.slice(length)
 }
 
+// 循环解析节点内容结束条件
+function isEnd(context:any, elementTagStack) {
+    // 2.当遇到结束标签时，结束
+    const s = context.source
+    console.log(s,'xxx', elementTagStack)
+    // if(parentTag && s.startsWith(`</${parentTag}>`)) { // 动态传入结束标签
+    //     return true
+    // }
+    // 如果当前是解析到结束标签，并匹配到闭合
+    if(s.startsWith('</')) {
+        // 判断当前闭合标签，是否出现元素标签栈中
+        for(let i = elementTagStack.length - 1; i >= 0; i--) {
+            const tag = elementTagStack[i].tag
+            if (s.slice(2, 2 + tag.length) ==  tag) {
+                return true
+            }
+        }
+    }
+    // 1.当context.source的内容推进完了，结束
+    return !s
+}
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -23,7 +23,8 @@ describe('parse', ()=>{
 
             expect(ast.children[0]).toStrictEqual({
                 type: NodeTypes.ELEMENT,
-                tag: 'div'
+                tag: 'div',
+                children: []
             })
         })
     })
@@ -37,5 +38,72 @@ describe('parse', ()=>{
                 content: 'some text'
             })
         })
+    })
+    test('hello world', () => {
+        const ast = baseParse("<p>hi,{{message}}</p>")
+        console.log(ast.children[0])
+        expect(ast.children[0]).toStrictEqual({
+            type: NodeTypes.ELEMENT,
+            tag: 'p',
+            children: [
+                {
+                    type: NodeTypes.TEXT,
+                    content: 'hi,'
+                },
+                {
+                    type: NodeTypes.INTERPOLATION,
+                    content: {
+                        type: NodeTypes.SIMPLE_EXPRESSION, // 简单表达式
+                        content: 'message'
+                    }
+                }
+            ]
+        })
+    })
+    test('hello world', () => {
+        const ast = baseParse("<div><p>xxx</p><div>hi,{{message}}</div></div>")
+        console.log(ast.children[0])
+        expect(ast.children[0]).toStrictEqual(
+            {
+                type: NodeTypes.ELEMENT,
+                tag: 'div',
+                children: [
+                    {
+                        type: NodeTypes.ELEMENT,
+                        tag: 'p',
+                        children: [
+                            {
+                                type: NodeTypes.TEXT,
+                                content: 'xxx'
+                            },
+                        ]
+                    },
+                    {
+                        type: NodeTypes.ELEMENT,
+                        tag: 'div',
+                        children: [
+                            {
+                                type: NodeTypes.TEXT,
+                                content: 'hi,'
+                            },
+                            {
+                                type: NodeTypes.INTERPOLATION,
+                                content: {
+                                    type: NodeTypes.SIMPLE_EXPRESSION, // 简单表达式
+                                    content: 'message'
+                                }
+                            }
+                        ]
+                    }
+                ],
+            }
+        )
+    })
+    test('hello world', () => {
+        // baseParse("<p><span></p>")
+        // 标签不闭合，异常情况
+        expect(()=>{
+            const ast = baseParse("<p><span></p>")
+        }).toThrow("标签不闭合")
     })
 })
